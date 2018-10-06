45bec0d624663ab7a2ce78010cc3b959fe100b19
ashley <kilgourkilgour@hotmail.com>
2018-10-06 20:51:53 +0100

    Added models

---
+++ EFModels/Model/SaleOrder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EFModels.Model
+{
+    public class SaleOrder
+    {
+        public Guid Id { get; set; }
+
+        public string CustomerFirstName { get; set; }
+
+        public string CustomerSecondName { get; set; }
+
+        public string Address { get; set; }
+
+        public virtual List<SaleOrderItem> SaleOrderItems { get; set; }
+    }
+}
---
+++ EFModels/Model/SaleOrderItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EFModels.Model
+{
+    public class SaleOrderItem
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public int Quantity { get; set; }
+
+        public double CostPerItem { get; set; }
+
+        public Guid SaleOrderId { get; set; }
+    }
+}
