def7e0d4e57b516a603b79be70d8bb061f3eaf52
ashley <kilgourkilgour@hotmail.com>
2018-10-06 21:43:45 +0100

    Set up context

---
+++ EFRepository/Context/MyContext.cs
@@ -0,0 +1,15 @@
+﻿using EFModels.Model;
+using System.Data.Entity;
+
+namespace EFRepository.Context
+{
+    public class MyContext : DbContext
+    {
+        public MyContext()
+        {
+        }
+
+        public IDbSet<SaleOrder> SaleOrder { get; set; }
+        public IDbSet<SaleOrderItem> SaleOrderItem { get; set; }
+    }
+}
